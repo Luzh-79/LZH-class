3d61283b86c2fbe15fd59156a2174601a188aa45
Luzh-79 <2755512675@qq.com>
2026-03-21 00:29:02 +0800

    完成作业：添加ex02和ex03代码

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # LZH-class
 我的作业
 这是我的第一个GitHub仓库，用于完成Git练习作业
+这是加入input程序的第二次修改
--- ex01/hello.c
+++ ex01/hello.c
@@ -1,5 +1,16 @@
-#include<stdio.h>
-int main(){
-	printf("Hello World!\n");
-	return 0;
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    printf("Hello World!\n");
+    
+    char user_input[100];
+    
+    printf("请输入一些内容: ");
+    fgets(user_input, sizeof(user_input), stdin);
+    user_input[strcspn(user_input, "\n")] = 0;
+    
+    printf("你输入的内容是: %s\n", user_input);
+    
+    return 0;
 }
